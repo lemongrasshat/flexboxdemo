55c3ced8e2d141ba9ecc7290bae5e305780ec59d
lemongrasshat <lemongrasshat@gmail.com>
2022-03-13 22:21:00 +0530

    site uploaded

---
+++ flex.js
@@ -0,0 +1,46 @@
+let justifyContentValue=document.querySelector("#jcontent");
+
+let alignItemsValue=document.querySelector("#aitems");
+
+let flexDirectionValue=document.querySelector("#fdir");
+
+let classToBeEdited="flex-container";
+const addButton=document.querySelector("#addItem");
+let addButtonEventHandler=()=>
+{
+    return ()=>{
+    let divClass=document.createElement("div");
+    divClass.classList.add("item");
+    let pTag=document.createElement("p");
+    let text=document.createTextNode("my item");
+    pTag.appendChild(text);
+    divClass.appendChild(pTag);
+    let containerClass=document.querySelector(".flex-container");
+    containerClass.appendChild(divClass);
+    }
+}
+
+let justifyHandler=()=>{
+
+    let newValue=document.querySelector("#jcontent").value;
+    let propertyFound=document.getElementsByClassName(classToBeEdited);
+    propertyFound[0].style.justifyContent=newValue;
+}
+let alignItemsHandler=()=>{
+
+    let newValue=document.querySelector("#aitems").value;
+    let propertyFound=document.getElementsByClassName(classToBeEdited);
+    propertyFound[0].style.alignItems=newValue;
+}
+let flexDirectionHandler=()=>{
+    let newValue=document.querySelector("#fdir").value;
+    let propertyFound=document.getElementsByClassName(classToBeEdited);
+    propertyFound[0].style.flexDirection=newValue;
+}
+
+justifyContentValue.addEventListener("change",justifyHandler)
+alignItemsValue.addEventListener("change",alignItemsHandler)
+flexDirectionValue.addEventListener("change",flexDirectionHandler)
+
+addButton.addEventListener('click',addButtonEventHandler())
+
